1addfd30fae5e97e968736e7f3d1ce7d653b4f9d
Pecke <102254036+ElPeckeLesta@users.noreply.github.com>
2023-08-29 18:15:54 -0300

    ej3.java

--- cadenadecaracteres/src/cadenadecaracteres/ej3.java
+++ cadenadecaracteres/src/cadenadecaracteres/ej3.java
@@ -1,33 +1,22 @@
-/* Realiza un programa que determine si una cadena de caracteres ingresada por el usuario es pal�ndromo 
+/* Realiza un programa que determine si una cadena de caracteres ingresada por el usuario es palíndromo 
  (se lee igual de izquierda a derecha que de derecha a izquierda). */
 package cadenadecaracteres;
 import java.util.Scanner;
 
 public class ej3 {
-
 	public static void main(String[] args) {
-		
-		System.out.print("<-> Ingresa una palabra y te voy a decir si es o no palindroma: ");
-		
+		System.out.print("Ingresá una palabra que quieras para ver si es palíndromo: ");
 		Scanner scanner = new Scanner(System.in);
-		
 		String palindromo = scanner.nextLine();
-		scanner.close();
-		
 		for(int i = 0; i < palindromo.length(); i++) {
-			for(int j = palindromo.length() - 1; j >= 0; j--) {
-				
-				char esPalindromoI = Character.toLowerCase(palindromo.charAt(i));
-				char esPalindromoJ = Character.toLowerCase(palindromo.charAt(j));
-				
-				if(esPalindromoI != esPalindromoJ) {
-					System.out.println("No es palindromo");
-					return; 
-				} 
-				break;
+			char letra1 = Character.toLowerCase(palindromo.charAt(i));
+			char letra2 = Character.toLowerCase(palindromo.charAt(palindromo.length()-1-i));
+			if(letra1 != letra2) {
+				System.out.print("Tu palabra '" + palindromo + "' no es un palíndromo");
+				return;
 			}
 		}
-			System.out.println("La palabra " + palindromo + " es palindroma");
+		System.out.print("Tu palabra '" + palindromo + "' es un palíndromo");
 	}
 
 }
